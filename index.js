d8b6d423852b18306582032a272522bd45072a67
Aakash <aakashpathrikar@gmail.com>
2025-06-29 17:29:47 +0530

    add dotenv for environment variable management and update .gitignore

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import 'dotenv/config'
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 let teaData = [];
@@ -17,39 +18,39 @@ app.post("/teas", (req, res) => {
 
 // routing
 app.get("/teas", (req, res) => {
-    res.status(200).send(teaData);
+  res.status(200).send(teaData);
 });
 
 // finding
 app.get("/teas/:id", (req, res) => {
-    const tea = teaData.find(t => t.id === parseInt(req.params.id));
-    if(!tea) {
-        return res.status(404).send("No such tea exists in the list");
-    }
-    res.status(200).send(tea);
-})
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("No such tea exists in the list");
+  }
+  res.status(200).send(tea);
+});
 
 // updating
 app.post("/teas/:id", (req, res) => {
-    const newTea = teaData.find((t) => t.id === parseInt(req.params.id));
-    if (!newTea) {
-      return res.status(404).send("No such tea exists in the list");
-    }
-    const {name, quantity, price} = req.body;
-    newTea.name = name;
-    newTea.quantity = quantity;
-    newTea.price = price;
-    res.status(200).send(newTea); 
+  const newTea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!newTea) {
+    return res.status(404).send("No such tea exists in the list");
+  }
+  const { name, quantity, price } = req.body;
+  newTea.name = name;
+  newTea.quantity = quantity;
+  newTea.price = price;
+  res.status(200).send(newTea);
 });
 
 // deleting
 app.delete("/teas/:id", (req, res) => {
-    const index = teaData.findIndex(t => t.id === parseInt(req.params.id));
-    if(index === -1) {
-        return res.status(404).send("Tea not fund");
-    }
-    teaData.splice(index, 1);
-    res.status(404).send("deleted");
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("Tea not fund");
+  }
+  teaData.splice(index, 1);
+  res.status(404).send("deleted");
 });
 
 app.listen(port, () => {
